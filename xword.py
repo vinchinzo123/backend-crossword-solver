f3acfca8e71b66ce9437f23f24831ed2ac1aa7e2
Vincent Newsom <vnewsom90@gmail.com>
2020-08-13 10:38:40 -0400

    finishes

--- xword.py
+++ xword.py
@@ -2,20 +2,27 @@
 # -*- coding: utf-8 -*-
 """Crossword Solver Program"""
 
-__author__ = "???"
+__author__ = "Vincent Newsom"
 
 # YOUR HELPER FUNCTION GOES HERE
+import re
 
 
 def main():
     with open('dictionary.txt') as f:
         words = f.read().split()
-
-    test_word = raw_input(
-        'Please enter a word to solve.\nUse spaces to signify unknown letters: ').lower()
+    test_word = input(
+        '''Please enter a word to solve.
+        \nUse spaces to signify unknown letters: ''').lower()
 
     # YOUR ADDITIONAL CODE HERE
-    raise NotImplementedError('Please complete this')
+    test_word = test_word.replace(" ", ".")
+    pattern = re.compile(test_word)
+    same_len_words = [word for word in words if pattern.match(
+        word) and len(word) == len(test_word)]
+    print(same_len_words)
+    for word in same_len_words:
+        print(word)
 
 
 if __name__ == '__main__':
